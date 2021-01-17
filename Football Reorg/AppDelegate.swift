9a4a5f8585b3c255523fef288bf5ffa38fabe74e
Poul Hornsleth <poulh@umich.edu>
2021-01-16 19:52:21 -0500

    Initial Commit

---
+++ Football Reorg/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Football Reorg
+//
+//  Created by Poul Hornsleth on 1/16/21.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Football Reorg/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Football Reorg
+//
+//  Created by Poul Hornsleth on 1/16/21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
